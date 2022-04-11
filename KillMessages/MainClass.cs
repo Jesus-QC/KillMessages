b5838f2791aa6ef33d3fd3b3ffb0a467ba961948
Jesús <69375249+Jesus-QC@users.noreply.github.com>
2022-04-11 15:59:48 +0200

    Initial commit

---
+++ KillMessages/MainClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs;
+using Player = Exiled.Events.Handlers.Player;
+
+namespace KillMessages
+{
+    public class MainClass : Plugin<PluginConfig>
+    {
+        public override string Name { get; } = "KillMessages";
+        public override string Prefix { get; } = "kill_messages";
+        public override string Author { get; } = "Jesus-QC";
+        public override Version Version { get; } = new (0, 0, 1);
+
+        public override void OnEnabled()
+        {
+            Player.Died += OnDied;
+            
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            Player.Died -= OnDied;
+            
+            base.OnDisabled();
+        }
+
+        private void OnDied(DiedEventArgs ev)
+        {
+            if(ev.Killer is null || ev.Target is null || ev.Target == ev.Killer)
+                return;
+            
+            ev.Killer.ShowHint(Config.Message.Replace("{name}", ev.Target.Nickname), 4);
+        }
+    }
+}
---
+++ KillMessages/PluginConfig.cs
@@ -0,0 +1,10 @@
+﻿using Exiled.API.Interfaces;
+
+namespace KillMessages
+{
+    public class PluginConfig : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        public string Message { get; set; } = "Killed <color=red>{name}</color>";
+    }
+}
